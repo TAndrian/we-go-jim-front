57d3496438c4930263736ba0afe9ea220e9d589e
TAndrian <LenOkimaru@gmail.com>
2024-07-06 02:50:41 +0200

    update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,25 @@
+// karma.conf.js
 module.exports = function(config) {
   config.set({
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+    files: [
+      'src/**/*.js',
+      'test/**/*.js'
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'src/**/*.js': ['coverage']
     },
+
+    // optionally, configure the reporter
     coverageReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
-      reporters: [
-        { type: 'lcov', subdir: '.' },
-        { type: 'text-summary' }
-      ]
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
-    autoWatch: false,
-    singleRun: true
+      type : 'html',
+      dir : 'coverage/'
+    }
   });
 };

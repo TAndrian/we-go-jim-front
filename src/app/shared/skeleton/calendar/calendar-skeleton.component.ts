531fc0d3d037c62757d66e0746f6188f2444912b
TAndrian <LenOkimaru@gmail.com>
2024-07-18 16:00:26 +0200

    feat(sidenavbar): Fix sonar Quality Gates

--- src/app/shared/skeleton/calendar/calendar-skeleton.component.ts
+++ src/app/shared/skeleton/calendar/calendar-skeleton.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-calendar-skeleton',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxSkeletonLoaderModule],
   templateUrl: './calendar-skeleton.component.html',
   styleUrl: './calendar-skeleton.component.scss'
 })
